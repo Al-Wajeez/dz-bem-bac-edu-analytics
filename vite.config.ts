a40ba938409b6bbc7c6b8f0a0d7b9c28dcdfac09
Haddadi Abdraouf <68353042+Al-Wajeez@users.noreply.github.com>
2025-07-02 22:55:46 +0100

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/",
+  base: process.env.VITE_BASE_PATH || "/dz-bem-bac-edu-analytics",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
